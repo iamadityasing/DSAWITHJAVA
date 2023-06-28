8870bb65783b0b93059e84fe3c61a289c759d705
iamadityasing <112313450+iamadityasing@users.noreply.github.com>
2023-06-28 18:12:17 +0530

    oj

--- aditya/constructor.java
+++ aditya/constructor.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class constructor{
     public static void main(String[] args) {
         Employee ac =new Employee();
-        //ac.setName("AdityaSingh");
+        //ac.setName("AdityaSinghh");
         //ac.setId(1);
         System.out.println(ac.getName()+"\n"+ac.getId());
     }
